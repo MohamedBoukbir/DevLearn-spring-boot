052dc3faa7e45e16e5d8da6987695e5af7451c73
[your <mohamed.boukbir@uit.ac.ma>
2025-02-18 21:47:56 +0100

    first commit

---
+++ src/main/java/com/devlearn/DevLearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLearnSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevLearnSpringBootApplication.class, args);
+    }
+
+}
